d7abb6e4f9e2859519dec1da71fb835a25e5520b
Anastasia Chizhikova <anastiva96@yandex.ru>
2023-02-24 21:07:25 +0300

    added program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string[] A;
+Console.Write("Введите массив строк через запятую: ");
+string array = Console.ReadLine();
+A = array.Split(',');
+var result = new string[A .Length];
+var Size = 0;
+foreach (var value in A )
+{
+    if (value.Length <= 3)
+    {
+        result[Size] = value;
+        Size++;
+    }
+}
+Console.Write(string.Join(Environment.NewLine, result, 0, Size));
+Console.ReadKey(true);
